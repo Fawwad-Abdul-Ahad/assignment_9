3d77450dd31be748725fc568b90020e4c078c185
Fawwad-Abdul-Ahad <fawwadabdulahad500@gmail.com>
2024-06-01 21:19:49 +0500

    flutter Assignment

---
+++ first_assignment/lib/profile_Screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Screen extends StatelessWidget {
+  const Screen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Text("Name : Fawwad \n  Skills : HTML, CSS JS, DART, Graphic Designing, \n  Hobbies : Cricket, Football, Cycling \n Profeesion : Software Engineer"),
+    );
+  }
+}
